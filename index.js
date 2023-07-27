f0ae5a080f4f044f0ab2f1462bca98c4be925571
Nevanje Kamuto <nevanjeka@gmail.com>
2023-07-27 13:18:47 +0200

    second commit

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/api/greet/:username', function(req, res){
 
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, function() {
     console.log(`app started on port ${PORT}`);
